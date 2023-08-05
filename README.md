aa53a609f1deffe79102d5c6c0e6a90b294ea7a5
Kartick Dey <kartick.d@exto360.com>
2023-08-05 14:01:18 +0530

    Added @angular/pwa package and setup basic stuffs

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Angular PWA (using service worker)
 
 Need to add @angular/pwa - `ng add @angular/pwa`
+
+Note: Need to install same verison as angular
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'angular-pwa';
+
+  public users: Array<any> = [];
+
+  private subscriptions = new Subscription();
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.subscriptions.add(
+      this.http
+        .get<Array<any>>('https://jsonplaceholder.typicode.com/users')
+        .subscribe((res) => {
+          console.log(res);
+          this.users = res;
+        })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
 }
